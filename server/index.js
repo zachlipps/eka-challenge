84e97a4df33de6b21a8e9c5ba43de6c476c992ab
Zach Lipps <zachlipps@Zachs-MBP.lan>
2017-11-13 20:52:07 -0800

    add file structure and create test route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,39 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+  path = require('path'),
+  cons = require('consolidate'),
+  bodyParser = require('body-parser'),
+  pg = require('pg'),
+  app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'onboarding',
+    password: 'joshx1d',
+    database: 'onboardingDB',
+    charset: 'utf8'
+  }
 });
 
-const PORT = process.env.PORT || 5000;
+var bookshelf = require('bookshelf')(knex);
+
+let User = bookshelf.Model.extend({ tableName: 'users' });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  new User({
+    username: 'guy',
+    password: 'guy1',
+    zip: 94034
+  }).save();
+  res.send({ hello: 'boddd' });
+});
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT);
